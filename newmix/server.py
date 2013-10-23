9ea87475e4cb86ea389e4a06d8ae9e960ae5abdf
Steve Crook <steve@mixmin.net>
2013-10-23 19:57:01 +0100

    Add some comments

--- newmix/server.py
+++ newmix/server.py
@@ -34,28 +34,55 @@ from daemon import Daemon
 
 class Server(Daemon):
     def run(self):
+        # Loop until a SIGTERM or Ctrl-C is received.
         while True:
-            process_inbound()
+            # Process outbound messages first.  This ensures that no message
+            # is received, processed and sent during the same iteration.  Not
+            # sure if doing so would be a bad thing for anonymity but not
+            # doing is it very unlikely to be bad.
             if out_pool.trigger():
                 process_outbound()
+            process_inbound()
+            # Some consideration should probably given to pool trigger times
+            # rather than stubbornly looping every minute.
             timing.sleep(60)
 
+
 def process_inbound():
+    """
+    Messages from other remailers are stored in the inbound pool.  These are
+    periodically processed and written to the outbound pool.  The exception to
+    this rule is when we are the final hop in a remailer chain.  In this
+    instance the message is delivered and not outbound queued.
+    """
     generator = in_pool.select_all()
     for filename in generator:
         with open(filename, 'r') as f:
             try:
                 packet_data = m.packet_read(f.read())
             except ValveError, e:
+                # ValueError is returned when the packet being processed isn't
+                # compliant with the specification.  These messages are
+                # deleted without further consideration.
                 log.debug("Newmix packet read failed with: %s", e)
                 in_pool.delete(filename)
                 continue
+            # Process the Base64 component of the message.
             m.decode(packet_data['binary'])
             in_pool.delete(filename)
             if m.is_exit:
+                #TODO Actually do something with exit messages
                 log.info("We got an exit message!!")
 
+
 def process_outbound():
+    """
+    Outbound messages are stored in a queue and a random subset of that queue
+    is processed each time this function is called (providing there are
+    sufficient messages queued to trigger sending.  No processing has to
+    be done prior to transmission.  This happens as part of the inbound queue
+    processing.
+    """
     generator = out_pool.select_subset()
     for filename in generator:
         with open(filename, 'r') as f:
@@ -66,18 +93,27 @@ def process_outbound():
                 out_pool.delete(filename)
                 continue
         if packet_data['expire'] < timing.epoch_days():
+            # Remailers come and go.  They also fail from time to time.  When
+            # a message is written to the outbound queue, it's stamped with an
+            # expiry date.  If it's still queued after that date, we give up
+            # trying to send it.  Sadly, a message is lost but messages can't
+            # be queued forever.
             log.warn("Giving up on sending msg to %s.",
                      packet_data['next_hop'])
+            #TODO Statistically mark down this remailer.
             out_pool.delete(filename)
             continue
         payload = {'newmix': packet_data['packet']}
         try:
+            # Actually try to send the message to the next_hop.  There are
+            # probably a lot of failure conditions to handle at this point.
             r = requests.post('http://%s/cgi-bin/webcgi.py'
                               % packet_data['next_hop'],
                               data=payload)
             if r.status_code == requests.codes.ok:
                 out_pool.delete(filename)
         except requests.exceptions.ConnectionError:
+            #TODO Mark down remailer statistics.
             log.info("Unable to connect to %s", packet_data['next_hop'])
 
 
@@ -96,16 +132,18 @@ if (__name__ == "__main__"):
     log.addHandler(handler)
 
     m = mix.Message()
-    in_pool = Pool.Pool(name = 'inpool',
-                        pooldir = config.get('pool', 'indir'))
-    out_pool = Pool.Pool(name = 'outpool',
-                         pooldir = config.get('pool', 'outdir'),
-                         interval = config.get('pool', 'interval'),
-                         rate = config.getint('pool', 'rate'),
-                         size = config.getint('pool', 'size'))
+    # The inbound pool always processes every message.
+    in_pool = Pool.Pool(name='inpool',
+                        pooldir=config.get('pool', 'indir'))
+    out_pool = Pool.Pool(name='outpool',
+                         pooldir=config.get('pool', 'outdir'),
+                         interval=config.get('pool', 'interval'),
+                         rate=config.getint('pool', 'rate'),
+                         size=config.getint('pool', 'size'))
     s = Server(config.get('general', 'pidfile'),
                stderr='/home/crooks/newmix/log/err.log')
 
+    # Handle command line args
     if len(sys.argv) >= 1:
         cmd = sys.argv[1]
         if cmd == "--start":
